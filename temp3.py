04e028a644a79f4f67fa250981755059574fc468
Your Name <you@example.com>
2019-10-06 22:56:44 +0800

    temp3.py

--- temp3.py
+++
@@ -1,9 +0,0 @@
-tempstr=input()
-if tempstr[0] in['C','c']:
-    f=eval(tempstr[1:-1])*1.8+32
-    print("F{:.2f}".format(f))
-elif tempstr[0] in['F','f']:
-    c=(eval(tempstr[1:-1])-32)/1.8
-    print("C{:.2f}".format(c))
-else:
-    print("格式错误")
