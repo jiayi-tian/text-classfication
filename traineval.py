9ddff4557e9b12826d63febfcff083419ac81075
Your Name <you@example.com>
2019-10-07 20:15:35 +0800

    训练

--- traineval.py
+++ traineval.py
@@ -33,11 +33,11 @@ def init_network(model, method='xavier', exclude='embedding', seed=123):
 num_epochs = 10 
 learning_rate = 1e-3
 batch_size = 128 #？？？
-require_improvement = 1000 
+#require_improvement = 1000 
 #save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'
 #class_list = [x.strip() for x in open(
             #dataset + '/data/class.txt').readlines()] 
-def train(model, train_iter, dev_iter, test_iter):
+def train(model, train_iter, test_iter):
     #start_time = time.time()
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -45,9 +45,9 @@ def train(model, train_iter, dev_iter, test_iter):
     # 学习率指数衰减，每次epoch：学习率 = gamma * 学习率
     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
     total_batch = 0  # 记录进行到多少batch
-    dev_best_loss = float('inf')
-    last_improve = 0  # 记录上次验证集loss下降的batch数
-    flag = False  # 记录是否很久没有效果提升
+    #dev_best_loss = float('inf')
+    #last_improve = 0  # 记录上次验证集loss下降的batch数
+    #flag = False  # 记录是否很久没有效果提升
     #writer = SummaryWriter(log_dir=config.log_path + '/' + time.strftime('%m-%d_%H.%M', time.localtime()))
     for epoch in range(num_epochs):
         print('Epoch [{}/{}]'.format(epoch + 1, num_epochs))
@@ -63,30 +63,30 @@ def train(model, train_iter, dev_iter, test_iter):
                 true = labels.data.cpu()
                 predic = torch.max(outputs.data, 1)[1].cpu()
                 train_acc = metrics.accuracy_score(true, predic)#所有分类正确的百分比，默认值为True，返回正确分类的比例；如果为False，返回正确分类的样本数
-                dev_acc, dev_loss = evaluate(model, dev_iter)#验证集多次使用，不断调参
-                if dev_loss < dev_best_loss:
+                #dev_acc, dev_loss = evaluate(model, dev_iter)#验证集多次使用，不断调参
+                '''if dev_loss < dev_best_loss:
                     dev_best_loss = dev_loss
                     torch.save(model.state_dict(),'train.pth')
                     improve = '*'
                     last_improve = total_batch
                 else:
                     improve = ''
-                #time_dif = get_time_dif(start_time)
-                msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%},  Val Loss: {3:>5.2},  Val Acc: {4:>6.2%}, {6}'
-                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc,improve))
+                #time_dif = get_time_dif(start_time)'''
+                msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%}, {6}'
+                print(msg.format(total_batch, loss.item(), train_acc, improve))
                 #writer.add_scalar("loss/train", loss.item(), total_batch)
                 #writer.add_scalar("loss/dev", dev_loss, total_batch)
                 #writer.add_scalar("acc/train", train_acc, total_batch)
                 #writer.add_scalar("acc/dev", dev_acc, total_batch)
                 model.train()
             total_batch += 1
-            if total_batch - last_improve >require_improvement:
+            '''if total_batch - last_improve >require_improvement:
                 # 验证集loss超过1000batch没下降，结束训练
                 print("No optimization for a long time, auto-stopping...")
                 flag = True
                 break
         if flag:
-            break
+            break'''
    #writer.close()
     test(model, test_iter)
 
@@ -94,7 +94,7 @@ def train(model, train_iter, dev_iter, test_iter):
 def test(model, test_iter):
     # test
     model.load_state_dict(torch.load('train.pth'))
-    model.eval()
+    #model.eval()
     #start_time = time.time()
     test_acc, test_loss, test_report, test_confusion = evaluate(model, test_iter, test=True)
     msg = 'Test Loss: {0:>5.2},  Test Acc: {1:>6.2%}'
@@ -108,7 +108,7 @@ def test(model, test_iter):
 
 
 def evaluate(model, data_iter, test=False):
-    model.eval()
+    #model.eval()
     loss_total = 0
     predict_all = np.array([], dtype=int)
     labels_all = np.array([], dtype=int)
